efe717f1eae4eb852eec5d5a1f7eb54451b4deb2
Alejandro R. Zaldivar <lexz.zaldivar@gmail.com>
2021-10-19 16:19:10 -0400

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -9,7 +9,7 @@ import { env } from './.env';
 export const environment = {
   production: true,
   version: env.npm_package_version,
-  serverUrl: 'http://68.183.128.50:8080/api/v1',
+  serverUrl: 'http://68.183.128.50/api/v1',
   defaultLanguage: 'en-US',
   supportedLanguages: ['en-US', 'es-ES'],
 };
