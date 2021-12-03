7ffb70ae8be23c2bf44720185daf1d9431766c9c
A.R.Z <lexz.zaldivar@gmail.com>
2021-12-03 06:55:25 -0500

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -9,7 +9,7 @@ import { env } from './.env';
 export const environment = {
   production: true,
   version: env.npm_package_version,
-  serverUrl: 'https://admin.cubanmusic.info/api/v1',
+  serverUrl: 'https://api.cubanmusic.info/api/v1',
   defaultLanguage: 'en-US',
   supportedLanguages: ['en-US', 'es-ES'],
 };
